a64f213c00cd1f20d879a91215f41d216a84c76d
Cameron Grieder <cjgrieder@gmail.com>
2021-12-03 08:22:04 -0700

    completed

--- sldc.js
+++ sldc.js
@@ -12,8 +12,22 @@
 // Planning
 // Testing & Integration
 
+// Answer:
+
+// planning
+// analysis of requirements
+// design
+// implementaion
+// testing and integration
+// maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+const planning = `creating and idea for sotware or additional feature`
+const analysisOfRequirements = `determine what is needed for the software or feature if regards to user interaction`
+const design = `the outline of the software is determined, the code languages that will be used`
+const implementation = `devs being building the software`
+const testingAndIntegration = `sofware or feature is tested to see if there are any issues`
+const maintain = `any issues found in testing are resolved, and updates are made to code as nessisary`
