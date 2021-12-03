a3370f1340b81018712bad2cd707bf915218fc30
Cameron Grieder <cjgrieder@gmail.com>
2021-12-03 13:21:27 -0700

    password validator

--- password.js
+++ password.js
@@ -6,7 +6,10 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-reader.question("What is your password");
-
-input.text('')
-
+reader.question("What is your password", function(answer){
+if (answer.length >= 10) {
+    console.log('password is valid')
+} else {
+    console.log('password not valid')
+}
+})
